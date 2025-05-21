7250866e54ecd2288387321dab4d60e37f93c42e
EarthlyEric <earthlyeric6@gmail.com>
2025-05-21 12:15:57 +0800

    add a image script

---
+++ create_docker_image.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/ayasa520/redroid-script.git
+cd redroid-script
+python -m venv venv
+venv/bin/pip install -r requirements.txt
+venv/bin/python redroid.py -a 11.0.0 -g -n

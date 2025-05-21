e1c86182f488ddfbca0c0b2a6be35b4b39080f81
EarthlyEric <earthlyeric6@gmail.com>
2025-05-21 13:44:25 +0800

    use android 12

--- create_docker_image.sh
+++ create_docker_image.sh
@@ -2,4 +2,4 @@ git clone https://github.com/ayasa520/redroid-script.git
 cd redroid-script
 python -m venv venv
 venv/bin/pip install -r requirements.txt
-venv/bin/python redroid.py -a 11.0.0 -g -n
+venv/bin/python redroid.py -a 12.0.0 -mtg -n

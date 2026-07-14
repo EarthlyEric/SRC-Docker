06cdf6a76c401ec843130cbbed352096434e32d5
EarthlyEric <earthlyeric6@gmail.com>
2026-07-14 21:42:05 +0800

    rewirte all dockerfile

--- create_docker_image.sh
+++ create_docker_image.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+clear
 git clone https://github.com/ayasa520/redroid-script.git
 cd redroid-script
-python -m venv venv
-venv/bin/pip install -r requirements.txt
-venv/bin/python redroid.py -a 11.0.0 -g -n
+uv init
+uv pip install -r requirements.txt
+uv run python redroid.py -a 11.0.0 -lg -n
